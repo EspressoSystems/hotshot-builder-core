77cfd5e63d7757fae6e8fe9092aab956fad9bb91
Himanshu Goyal <goyal.himanshu1799@gmail.com>
2024-01-14 13:06:58 -0500

    remove tasks dir

--- src/data_source.rs
+++ src/data_source.rs
@@ -17,6 +17,33 @@ fn process_external_transaction(){
 
 }
 
-fn process_hotshot_transaction(){
+// fn process_hotshot_transaction(){
 
+// }
+// use hotshot_task::{
+//     boxed_sync,
+//     event_stream::ChannelStream,
+//     task::{FilterEvent, HandleEvent, HandleMessage, HotShotTaskCompleted, HotShotTaskTypes},
+//     task_impls::TaskBuilder,
+//     task_launcher::TaskRunner,
+//     GeneratedStream, Merge,
+// };
+use hotshot_types::traits::node_implementation::{NodeImplementation, NodeType};
+
+
+// process the hotshot transaction event 
+pub async fn process_hotshot_transaction<Types: NodeType, I: NodeImplementation<TYPES>>(
+    event_stream: ChannelStream<HotShotEvent<Types>>,
+    handle: SystemContextHandle<Types, I>,
+) -> TaskRunner
+{
+    let transactions_event_handler = HandleEvent(Arc::new(
+        move |event, mut state: TransactionTaskState<TYPES, I, HotShotConsensusApi<TYPES, I>>| {
+            async move {
+                let completion_status = state.handle_event(event).await;
+                (completion_status, state)
+            }
+            .boxed()
+        },
+    ));
 }
