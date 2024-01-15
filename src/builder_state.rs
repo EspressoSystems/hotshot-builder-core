6eb1456f7d1f87e191552f18cd8a56b995cbe8a4
Himanshu Goyal <goyal.himanshu1799@gmail.com>
2024-01-14 20:45:55 -0500

    add external tx receipt

--- src/builder_state.rs
+++ src/builder_state.rs
@@ -4,7 +4,8 @@ use std::hash::BuildHasher;
 //use std::error::Error;
 use std::sync::{Arc, Mutex};
 use futures::Future;
-use unix_time::Instant;
+use hotshot_types::traits::block_contents::Transaction;
+use std::time::Instant;
 use async_trait::async_trait;
 enum TransactionType {
     External, // txn from the external source i.e private mempool
@@ -12,9 +13,17 @@ enum TransactionType {
 }
 
 pub trait BuilderType {
-    type TransactionID;
+    type TransactionID; // currently using the transaction commit as the transaction id
     type Transaction;
-    type TransactionCommit;
+    type TransactionCommit: std::cmp::PartialOrd
+    + std::cmp::Ord
+    + std::cmp::Eq
+    + std::cmp::PartialEq
+    + std::hash::Hash
+    + Clone
+    + Send
+    + Sync
+    + 'static;
     type Block;
     type BlockHeader;
     type BlockPayload;
@@ -31,15 +40,17 @@ pub trait BuilderType {
 // }
 
 pub struct BuilderState<T: BuilderType> {
-    transactionspool: Arc<Mutex<BTreeMap<Instant, HashMap<T::TransactionID,T::Transaction>>>>,
-    processed_blocks: Arc<Mutex<HashMap<T::BlockCommit, Vec<T::Block>>>>,
-    processed_views: Arc<Mutex<HashMap<T::ViewNum, HashSet<T::Block>>>>,
+    pub time_to_txid: Arc<Mutex<BTreeMap<Instant, HashSet<T::TransactionCommit>>>>,
+    pub txid_to_tx: Arc<Mutex<HashMap<T::TransactionCommit,T::Transaction>>>,
+    pub processed_blocks: Arc<Mutex<HashMap<T::BlockCommit, Vec<T::Block>>>>,
+    pub processed_views: Arc<Mutex<HashMap<T::ViewNum, HashSet<T::Block>>>>,
 }
 
 impl<T:BuilderType> BuilderState<T>{
     fn new()->BuilderState<T>{
        BuilderState {
-                   transactionspool: Arc::new(Mutex::new(BTreeMap::new())),
+                   time_to_txid: Arc::new(Mutex::new(BTreeMap::new())),
+                   txid_to_tx: Arc::new(Mutex::new(HashMap::new())),
                    processed_blocks: Arc::new(Mutex::new(HashMap::new())),
                    processed_views: Arc::new(Mutex::new(HashMap::new())),
                }
--- src/data_source.rs
+++ src/data_source.rs
@@ -38,8 +38,10 @@ use hotshot_types::{
 };
 use hotshot_types::{data::Leaf, simple_certificate::QuorumCertificate};
 
-use std::sync::Arc;
+use std::{sync::Arc, time::Instant, collections::HashSet};
 use tracing::error;
+
+use crate::builder_state::{BuilderState, BuilderType};
 
 
 // process the hotshot transaction event 
@@ -61,6 +63,36 @@ pub async fn process_hotshot_transaction<Types: NodeType, I: NodeImplementation<
 
 
 // Used by the third API service i.e. to submit a transaction externally (priavate mempool)
-async fn process_external_transaction(){
-    unimplemented!();
+async fn process_external_transaction<T:BuilderType>(builder_info: &mut BuilderState<T>, tx_hash: T::TransactionCommit, tx: T::Transaction){
+    // go through the builderinfo.transactionspool btree map check if it already exits based on its transaction hash
+    // if it does not exist, then add it to the builderinfo.transactionspool btree map
+    // if it exists, then ignore it
+
+    if let Ok(mut txid_to_tx) = builder_info.txid_to_tx.lock() {
+        if txid_to_tx.contains_key(&tx_hash) {
+            println!("Transaction already exists in the builderinfo.txid_to_tx hashmap");
+        } else {
+            // get the current time
+            let current_time = Instant::now();
+            // now check if the current time already exists in the builderinfo.time_to_txid btree map
+            if let Ok(mut time_to_txid) = builder_info.time_to_txid.lock(){
+                // if it exists, then add the transaction hash to the existing set
+                if time_to_txid.contains_key(&current_time) {
+                    let mut existing_set = time_to_txid.get(&current_time).unwrap().clone();
+                    existing_set.insert(tx_hash.clone());
+                    time_to_txid.insert(current_time, existing_set);
+                } else {
+                    // if it does not exist, then create a new set and add the transaction hash to it
+                    let mut new_set = HashSet::new();
+                    new_set.insert(tx_hash.clone());
+                    time_to_txid.insert(current_time, new_set);
+                }
+            }
+
+            txid_to_tx.insert(tx_hash, tx);
+            
+        }
+    }
+        // Code to handle when the transaction hash exists in the map
 }
+      
