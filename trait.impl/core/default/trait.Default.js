1e618817424137653b627a6d15f0fc939c47e1f3
nyospe <nyospe@users.noreply.github.com>
2024-03-22 23:04:26 +0000

    deploy: 4cdf4cf78d4035341d46042099abec9da230ebff

--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hs_builder_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"hs_builder_core/service/struct.Options.html\" title=\"struct hs_builder_core::service::Options\">Options</a>"]]
+"hs_builder_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"hs_builder_core/service/struct.Options.html\" title=\"struct hs_builder_core::service::Options\">Options</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
