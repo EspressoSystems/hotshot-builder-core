a363ca59c635b2b4d6dabad0255ed5c4bc12811c
move47 <move47@users.noreply.github.com>
2024-05-02 23:18:27 +0000

    deploy: 546b25bd66840994af3c85ff5c0ecca7db0920e1

--- hotshot_builder_core/service/sidebar-items.js
+++ hotshot_builder_core/service/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["RecvError","TryRecvError"],"fn":["broadcast","run_non_permissioned_standalone_builder_service","run_permissioned_standalone_builder_service"],"struct":["GlobalState","ProxyGlobalState"]};
+window.SIDEBAR_ITEMS = {"enum":["RecvError","TryRecvError"],"fn":["broadcast","run_non_permissioned_standalone_builder_service","run_permissioned_standalone_builder_service"],"struct":["BlockInfo","BuilderStatesInfo","GlobalState","ProposedBlockId","ProxyGlobalState"]};
---
+++ trait.impl/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"hotshot_builder_core":[["impl&lt;Types: <a class=\"trait\" href=\"hotshot_builder_core/testing/basic_test/trait.NodeType.html\" title=\"trait hotshot_builder_core::testing::basic_test::NodeType\">NodeType</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"hotshot_builder_core/service/struct.BuilderStatesInfo.html\" title=\"struct hotshot_builder_core::service::BuilderStatesInfo\">BuilderStatesInfo</a>&lt;Types&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
